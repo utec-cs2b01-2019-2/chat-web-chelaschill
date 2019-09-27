e0b7fa2f72b6dac1a637acee234d0bc8733bf6a5
alvaro aguirre <44936171+chelaschill@users.noreply.github.com>
2019-09-27 09:18:29 -0500

    Update server.py

--- web/server.py
+++ web/server.py
@@ -20,7 +20,8 @@ def static_content(content):
 
 @app.route('/users', methods = ['POST'])
 def create_user():
-    c =  json.loads(request.form['values'])
+    #c = json.loads(request.form['values'])
+    c = json.loads(request.data)
     user = entities.User(
         username=c['username'],
         name=c['name'],
@@ -50,21 +51,23 @@ def get_users():
     data = dbResponse[:]
     return Response(json.dumps(data, cls=connector.AlchemyEncoder), mimetype='application/json')
 
-@app.route('/users', methods = ['PUT'])
-def update_user():
+@app.route('/users/<id>', methods = ['PUT'])
+def update_user(id):
     session = db.getSession(engine)
-    id = request.form['key']
+    #id = request.form['key']
     user = session.query(entities.User).filter(entities.User.id == id).first()
-    c = json.loads(request.form['values'])
+    #c = json.loads(request.form['values'])
+    c = json.loads(request.data)
     for key in c.keys():
         setattr(user, key, c[key])
     session.add(user)
     session.commit()
     return 'Updated User'
 
-@app.route('/users', methods = ['DELETE'])
-def delete_user():
-    id = request.form['key']
+@app.route('/users/<id>', methods = ['DELETE'])
+def delete_user(id):
+    #id = request.form['key']
+
     session = db.getSession(engine)
     user = session.query(entities.User).filter(entities.User.id == id).one()
     session.delete(user)
@@ -228,18 +231,90 @@ def suma(numero):
     session['suma'] = suma
     return str(suma)
 
+@app.route('/login', methods = ['POST'])
+def todos_los_usuarios():
+    db_session = db.getSession(engine)
+    users = db_session.query(entities.User)
+    response = ""
+    for user in users:
+        response+= user.username + " - " + user.password+";"
+    return response
+
+
 #Stateful interaction
 @app.route('/login', methods = ['POST'])
 def login():
     usuario = request.form['usuario']
     password = request.form['password']
-    if usuario == 'alvaro' and password == '12345':
+    db_session = db.getSession(engine)
+    user = db_session.query(entities.User).filter(
+        entities.User.username == usuario
+    ).filter(
+        entities.User.password == password
+    ).first()
+
+    if usuario != None:
         session['usuario'] = usuario
-        return 0
+        return "Welcome " + usuario
     else:
         return "Lo siento " + usuario + " no eres un usuario valido"
 
+#API DE GRUPOS
+#1. CREATE
 
+@app.route('/groups', methods = ['POST'])
+def create_group():
+    c = json.loads(request.data)
+    group = entities.Group(
+        name = c['name']
+    )
+    session_db = db.getSession(engine)
+    session_db.add(group)
+    session_db.commit()
+    return 'Created Group'
+
+
+#2. READ
+
+@app.route('/groups/<id>', methods = ['GET'])
+def read_group(id):
+
+    session_db = db.getSession(engine)
+    group = session_db.query(entities.Group).filter(
+        entities.Group.id == id).first()
+    data = json.dumps(group, cls=connector.AlchemyEncoder)
+    return Response(data, status = 200, mimetype='application/json')
+
+@app.route('/groups', methods = ['GET'])
+def get_all_groups():
+
+    session_db = db.getSession(engine)
+    dbResponse = session_db.query(entities.Group)
+    data = dbResponse[:]
+    return Response(json.dumps(data, cls = connector.AlchemyEncoder), mimetype = 'application/json')
+
+#UPDATE
+@app.route('/groups/<id>', methods = ['PUT'])
+def update_group(id):
+    session = db.getSession(engine)
+    group = session.query(entities.Group).filter(entities.Group.id == id).first()
+
+    c = json.loads(request.data)
+    for key in c.keys():
+        setattr(group, key, c[key])
+    session.add(group)
+    session.commit()
+    return 'Updated Group'
+
+#DELETE
+@app.route('/groups/<id>', methods = ['DELETE'])
+def delete_group(id):
+
+    session = db.getSession(engine)
+    group = session.query(entities.Group).filter(entities.Group.id == id).one()
+    session.delete(group)
+    session.commit()
+    return "Deleted Group"
 
 if __name__ == '__main__':
     app.secret_key = ".."
